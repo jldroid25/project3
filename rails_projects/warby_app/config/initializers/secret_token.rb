0aca6c103ccc0c2c0bfaa6c1234174fac3b775bc
jimad12 <jamesbong12@gmail.com>
2012-01-07 16:17:15 -0500

    Initial commit

---
+++ rails_projects/warby_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WarbyApp::Application.config.secret_token = 'a337b370ced3d6762506cf65cb84869bc10d83f1c294aff93eb91b300e02e82801b1dffe47a1f40522d60b2b326083a06ef0d87a35e2c85ca9155b9fa584b379'
