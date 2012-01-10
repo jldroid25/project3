f4548f0e5752d9dfaa5003026d360902af2c8238
james Lemaire <jamesbong12@gmail.com>
2012-01-10 00:04:33 -0500

    Finished first cut of the User model

--- rails_projects/warby_app/app/controllers/users_controller.rb
+++ rails_projects/warby_app/app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
   def new
   end
+  
+  def help
+    @title ="Help"
+  end
+  
+  def new
+    @title = "Sign up"
+    
+    def show 
+      @user = User.find(params[:id])
+    end
+    
+    def new
+      @title = "Sign up"
+    
+  end
 
 end
--- rails_projects/warby_app/app/helpers/application_helper.rb
+++ rails_projects/warby_app/app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def logo
+    # file in.
+end
+
+# return a title on a per-page basis.
+def title
+end
 end
